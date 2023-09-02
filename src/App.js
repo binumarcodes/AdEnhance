a40d76eb2593a2b652e2885142cb9b3499965864
binumarcodes <abdulrahmanbinumar178@gmail.com>
2023-09-02 10:57:27 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Topbar from './components/Topbar'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <Sidebar />
+      
+    </div>
+  );
+}
+
+export default App;
