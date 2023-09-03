02c8a1fdd34175a2ec5033a33ab2cb710cf735bc
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2023-09-03 13:49:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import Topbar from './components/Topbar'
-import Sidebar from './components/Sidebar'
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
+      <ResponsiveAppBar />
       <Sidebar />
-      
     </div>
   );
 }
